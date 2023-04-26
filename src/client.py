9af31fe23c9480ef31794d519b7b6b870dd665d1
Mike Brouwer <mikebrouwer@happyhorizon.com>
2023-04-26 22:22:41 +0200

    :sparkles: Now using lock files

--- src/client.py
+++ src/client.py
@@ -14,11 +14,14 @@ PUSHER_CLUSTER = os.getenv('PUSHER_CLUSTER')
 PUSHER_WEBSOCKET_URL = f'wss://ws-{PUSHER_CLUSTER}.pusher.com/app/{PUSHER_APP_KEY}?client=js&version=7.0.3&protocol=5'
 LOCAL_USER = os.getenv('LOCAL_USER')
 NVIDIA_KEY_COMBO = os.getenv('NVIDIA_KEY_COMBO')
+LOCK_FILE = "./lockfile.lock"
 
 def save_replay(name):
+    global LOCK_FILE
     now = datetime.datetime.now()
     formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
     print(f'[{formatted_time}] Replay saved by {name}!')
+    os.touch(LOCK_FILE)
     keyboard.press_and_release(NVIDIA_KEY_COMBO)
 
 async def client():
--- src/server.py
+++ src/server.py
@@ -16,46 +16,54 @@ LOCAL_USER = os.getenv('LOCAL_USER')
 LOCAL_USER_IMAGE = os.getenv('LOCAL_USER_IMAGE')
 IMGBB_API_KEY = os.getenv('IMGBB_API_KEY')
 DISCORD_WEBHOOK = os.getenv('DISCORD_WEBHOOK')
+LOCK_FILE = "./lockfile.lock"
 
 pusher_client = pusher.Pusher(
-  app_id=os.getenv('PUSHER_APP_ID'),
-  key=os.getenv('PUSHER_KEY'),
-  secret=os.getenv('PUSHER_SECRET'),
-  cluster=os.getenv('PUSHER_CLUSTER'),
-  ssl=os.getenv('PUSHER_SSL', True)
+    app_id=os.getenv('PUSHER_APP_ID'),
+    key=os.getenv('PUSHER_KEY'),
+    secret=os.getenv('PUSHER_SECRET'),
+    cluster=os.getenv('PUSHER_CLUSTER'),
+    ssl=os.getenv('PUSHER_SSL', True)
 )
 
+if os.path.exists(LOCK_FILE):
+    os.remove(LOCK_FILE)
+
 def send_save_replay():
-  global IS_SENDING, LOCAL_USER
+    global IS_SENDING, LOCAL_USER, LOCK_FILE
 
-  if IS_SENDING:
-    return
+    if os.path.exists(LOCK_FILE):
+        os.remove(LOCK_FILE)
+        return
 
-  IS_SENDING = True
-  now = datetime.datetime.now()
-  formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    if IS_SENDING:
+        return
 
-  print(f'[{formatted_time}] Sending save replay to the others...')
-  pusher_client.trigger('lan-party', 'save-replay', {'from': LOCAL_USER})
-  screenshot()
+    IS_SENDING = True
+    now = datetime.datetime.now()
+    formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
-  time.sleep(5)
-  IS_SENDING = False
+    print(f'[{formatted_time}] Sending save replay to the others...')
+    pusher_client.trigger('lan-party', 'save-replay', {'from': LOCAL_USER})
+    screenshot()
+
+    time.sleep(5)
+    IS_SENDING = False
 
 def screenshot():
-  with mss.mss() as sct:
-    sct.shot(output="last_screenshot.png")
+    with mss.mss() as sct:
+        sct.shot(output="last_screenshot.png")
 
-  url = "https://api.imgbb.com/1/upload"
-  payload = {"key": IMGBB_API_KEY}
-  files = {"image": open("../last_screenshot.png", "rb")}
-  response = requests.post(url, payload, files=files)
+    url = "https://api.imgbb.com/1/upload"
+    payload = {"key": IMGBB_API_KEY}
+    files = {"image": open("./last_screenshot.png", "rb")}
+    response = requests.post(url, payload, files=files)
 
-  json_response = json.loads(response.text)
-  img_url = json_response["data"]["url"]
+    json_response = json.loads(response.text)
+    img_url = json_response["data"]["url"]
 
-  print("Screenshot uploaded to: " + img_url)
-  send_discord_screenshot(img_url)
+    print("Screenshot uploaded to: " + img_url)
+    send_discord_screenshot(img_url)
 
 def send_discord_screenshot(image_url):
     global DISCORD_WEBHOOK, LOCAL_USER, LOCAL_USER_IMAGE
