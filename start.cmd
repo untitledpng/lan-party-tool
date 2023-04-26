bed4b9406106d89efd0d6b371dd91d01511402ab
Mike Brouwer <mikebrouwer@happyhorizon.com>
2023-04-26 21:40:25 +0200

    :sparkles: Now using the improved start commands

--- start.cmd
+++ start.cmd
@@ -1,1 +1,5 @@
-python ./src/main.py
+@echo off
+for /f "tokens=1* delims==" %%a in ('type .\src\.env ^| findstr /b PYTHON_EXECUTABLE=') do set "python_executable=%%b"
+
+cd ./src
+%python_executable% ./main.py
--- update.cmd
+++ update.cmd
@@ -1,1 +1,4 @@
-python ./src/self-update.py
+@echo off
+for /f "tokens=1* delims==" %%a in ('type .\src\.env ^| findstr /b PYTHON_EXECUTABLE=') do set "python_executable=%%b"
+
+%python_executable% ./src/self-update.py
