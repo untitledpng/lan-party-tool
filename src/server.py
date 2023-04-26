e4172e83ae8adf6a14766b858a8ffb53d689054d
Mike Brouwer <mikebrouwer@happyhorizon.com>
2023-04-26 21:31:41 +0200

    :sparkles: Improved screenshot code

--- src/server.py
+++ src/server.py
@@ -3,8 +3,6 @@ import keyboard
 import datetime
 import time
 import mss
-from PIL import Image
-import io
 import requests
 import json
 import os
@@ -45,37 +43,19 @@ def send_save_replay():
   IS_SENDING = False
 
 def screenshot():
-  # Take a fullscreen screenshot
   with mss.mss() as sct:
-    # Get information about the primary monitor
-    monitor = sct.monitors[1]
+    sct.shot(output="last_screenshot.png")
 
-    # Capture the entire screen
-    screenshot = sct.grab(monitor)
-
-  # Convert the screenshot to a PIL Image object
-  img = Image.frombytes("RGB", screenshot.size, screenshot.raw, "raw", "BGRX")
-
-  # Save the screenshot to disk
-  buffer = io.BytesIO()
-  img.save(buffer, format="PNG")
-  with open("../screenshot.png", "wb") as f:
-    f.write(buffer.getvalue())
-
-  # Upload the screenshot to ImgBB
   url = "https://api.imgbb.com/1/upload"
   payload = {"key": IMGBB_API_KEY}
-  files = {"image": open("../screenshot.png", "rb")}
+  files = {"image": open("../last_screenshot.png", "rb")}
   response = requests.post(url, payload, files=files)
 
-  # Parse the JSON response to get the URL of the uploaded image
   json_response = json.loads(response.text)
   img_url = json_response["data"]["url"]
 
-  # Print the URL of the uploaded image
   print("Screenshot uploaded to: " + img_url)
   send_discord_screenshot(img_url)
-
 
 def send_discord_screenshot(image_url):
     global DISCORD_WEBHOOK, LOCAL_USER, LOCAL_USER_IMAGE
