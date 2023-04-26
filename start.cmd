b01039a0033d1abdd25631456db5e36386dde77d
Mike Brouwer <mikebrouwer@happyhorizon.com>
2023-04-26 21:49:53 +0200

    :bug: Fixed problems with loading python main

--- start.cmd
+++ start.cmd
@@ -1,5 +1,4 @@
 @echo off
-for /f "tokens=1* delims==" %%a in ('type .\src\.env ^| findstr /b PYTHON_EXECUTABLE=') do set "python_executable=%%b"
 
-cd ./src
-%python_executable% ./main.py
+cd src
+python main.py
