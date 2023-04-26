4c49b4fe5c6b7269f5f3b6604f61d28ef30be2b0
Mike Brouwer <mikebrouwer@happyhorizon.com>
2023-04-26 21:42:13 +0200

    :truck: Renamed self_update

--- update.cmd
+++ update.cmd
@@ -1,4 +1,4 @@
 @echo off
 for /f "tokens=1* delims==" %%a in ('type .\src\.env ^| findstr /b PYTHON_EXECUTABLE=') do set "python_executable=%%b"
 
-%python_executable% ./src/self-update.py
+%python_executable% ./src/self_update.py
